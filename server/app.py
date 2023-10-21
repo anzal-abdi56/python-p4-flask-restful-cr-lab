013a5c9c1450037e7734c083a3657fabf2a10dc1
+ <+>
2023-10-21 15:03:54 +0300

    Complete

--- server/app.py
+++ server/app.py
@@ -4,7 +4,7 @@ from flask import Flask, jsonify, request, make_response
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
 
-from models import db, Plant
+from models import db,Plant
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///plants.db'
@@ -17,11 +17,48 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    def get(self):
+        plants_list=[plant.to_dict() for plant in Plant.query.all()]
+        response=make_response(
+            jsonify(plants_list),
+            200,
+        )
+        return response
+    def post(self):
+        new_plant=Plant(
+            name=request.get_json()["name"],
+            image=request.get_json()["image"],
+            price=request.get_json()["price"]
+        )
+        db.session.add(new_plant)
+        db.session.commit()
+
+        new_plant_dict = new_plant.to_dict()
+
+        response = make_response(
+            jsonify(new_plant_dict),
+            201,
+        )
+        response.headers["Content-Type"] = "application/json"
+        return response
+    
+
+api.add_resource(Plants,'/plants')
 
 class PlantByID(Resource):
-    pass
+    def get(self,id):
+        
+        my_plant_dict = Plant.query.filter_by(id=id).first().to_dict()
+
+        response = make_response(
+            jsonify(my_plant_dict),
+            200,
+        )
+        return response
+    
+    
+api.add_resource(PlantByID,'/plants/<int:id>')
         
 
 if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+    app.run(port=5550, debug=True)
